8638d23fab0d6b9664b38fa387659aa4adfe5e1a
viktornagy <vnagy2000@gmail.com>
2017-01-26 11:49:44 +0100

    Project start 2

---
+++ src/main/java/com/greenfox/fedex/NonogramMain.java
@@ -0,0 +1,10 @@
+package com.greenfox.fedex;
+
+/**
+ * Created by Viktor on 2017.01.26..
+ */
+public class NonogramMain {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
