cf4f0c2cfbcab1274fc2c41c74791cdf66604015
vikukzs <vikuk.zs@gmail.com>
2017-01-26 21:30:08 +0100

    marci hozzanyult a gepemhez

--- src/main/java/com/greenfox/fedex/model/NBlocksTableModel.java
+++ src/main/java/com/greenfox/fedex/model/NBlocksTableModel.java
@@ -15,4 +15,18 @@ public class NBlocksTableModel extends DefaultTableModel {
             for (int column = 0; column < numberOfColumns; column++)
                 setValueAt(new Integer(0), row, column);
     }
+
+    public NBlocksTableModel(int numberOfRows, int numberOfColumns, NPuzzle puzzle, Boolean rows) {
+        super(numberOfRows, numberOfColumns);
+
+		/* Initialize blocks cells. */
+        for (int row = 0; row < numberOfRows; row++)
+            for (int column = 0; column < numberOfColumns; column++) {
+                if (rows)
+                    setValueAt(0, row, column);
+                else
+                    setValueAt(0, row, column);
+            }
+    }
+
 }
