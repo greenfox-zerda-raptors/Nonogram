9b243ade7fe09f2f11724d8058b9f7829f5fcf1c
vikukzs <vikuk.zs@gmail.com>
2017-01-26 15:35:09 +0100

    add grid stuff

--- src/main/java/com/greenfox/fedex/NonogramMain.java
+++ src/main/java/com/greenfox/fedex/NonogramMain.java
@@ -1,5 +1,7 @@
 package com.greenfox.fedex;
 
+import com.greenfox.fedex.model.NTableModel;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,6 +15,9 @@ public class NonogramMain extends JFrame {
     JMenu menu = new JMenu("File");
     JMenuItem menuItem = new JMenuItem("Exit");
 
+    JTable crosswordTable = new JTable(new NTableModel(10, 10));
+    private GridBagConstraints constraints = new GridBagConstraints();
+
     GridBagLayout gridBagLayout = new GridBagLayout();
 
     Toolkit tk = Toolkit.getDefaultToolkit();
@@ -23,13 +28,13 @@ public class NonogramMain extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        this.setSize(400, 400);
-
+        setResizable(false);
         Dimension dim = tk.getScreenSize();
         int xPos = (dim.width / 2) - (this.getWidth() / 2);
         int yPos = (dim.height / 2) - (this.getHeight() / 2);
         this.setLocation(xPos, yPos);
         this.add(mainPanel);
+        setupTable();
         setJMenuBar(menubar);
         menubar.add(menu);
         menu.add(menuItem);
@@ -40,6 +45,14 @@ public class NonogramMain extends JFrame {
             }
         });
 
+        pack();
+    }
+
+    public void setupTable() {
+
+        constraints.gridx = 1; constraints.gridy = 1;
+        constraints.insets = new Insets(0, 0, 0, 0);
+        mainPanel.add(crosswordTable, constraints);
     }
 
     public static void main(String[] args) {
