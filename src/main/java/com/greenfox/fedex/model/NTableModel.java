57a285c48a1d12e8b28fedda8cd45bfa4e310cd8
vikukzs <vikuk.zs@gmail.com>
2017-01-26 14:58:49 +0100

    add table model and constructors

---
+++ src/main/java/com/greenfox/fedex/model/NTableModel.java
@@ -0,0 +1,27 @@
+package com.greenfox.fedex.model;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Created by Zsuzska on 2017. 01. 26..
+ */
+public class NTableModel extends DefaultTableModel {
+
+    public NTableModel(int numberOfRows, int numberOfColumns) {
+        super(numberOfRows, numberOfColumns);
+
+        for (int row = 0; row < numberOfRows; row++)
+            for (int column = 0; column < numberOfColumns; column++)
+                setValueAt(new NTableCell(), row, column);
+    }
+
+    public NTableModel(int numberOfRows, int numberOfColumns, boolean[][] inputArray) {
+        super(numberOfRows, numberOfColumns);
+
+        for (int row = 0; row < numberOfRows; row++)
+            for (int column = 0; column < numberOfColumns; column++)
+                setValueAt(new NTableCell(inputArray[row][column] ? NTableCell.EMPTY : NTableCell.ON), row, column);
+    }
+
+
+}
