9241c3321b09fe5043fbfa89cb9eb1a62c6729d5
vikukzs <vikuk.zs@gmail.com>
2017-01-26 14:10:47 +0100

    add board base

--- src/main/java/com/greenfox/fedex/NonogramMain.java
+++ src/main/java/com/greenfox/fedex/NonogramMain.java
@@ -1,10 +1,52 @@
 package com.greenfox.fedex;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by Viktor on 2017.01.26..
  */
-public class NonogramMain {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+public class NonogramMain extends JFrame {
+    JMenuBar menubar = new JMenuBar();
+    JMenu menu = new JMenu("File");
+    JMenuItem menuItem = new JMenuItem("Exit");
+
+    GridBagLayout gridBagLayout = new GridBagLayout();
+
+    Toolkit tk = Toolkit.getDefaultToolkit();
+    JPanel mainPanel = new JPanel(gridBagLayout);
+
+    public NonogramMain() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setSize(400, 400);
+
+        Dimension dim = tk.getScreenSize();
+        int xPos = (dim.width / 2) - (this.getWidth() / 2);
+        int yPos = (dim.height / 2) - (this.getHeight() / 2);
+        this.setLocation(xPos, yPos);
+        this.add(mainPanel);
+        setJMenuBar(menubar);
+        menubar.add(menu);
+        menu.add(menuItem);
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
     }
-}
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new NonogramMain();
+            }
+        });
+    }}
