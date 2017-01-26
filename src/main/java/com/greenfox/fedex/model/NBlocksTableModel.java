f8d18e3714238bd867d1e092a5822bf00fdf6448
vikukzs <vikuk.zs@gmail.com>
2017-01-26 20:19:59 +0100

    add blocks table and model class

---
+++ src/main/java/com/greenfox/fedex/model/NBlocksTableModel.java
@@ -0,0 +1,18 @@
+package com.greenfox.fedex.model;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Created by Zsuzska on 2017. 01. 26..
+ */
+public class NBlocksTableModel extends DefaultTableModel {
+
+    public NBlocksTableModel(int numberOfRows, int numberOfColumns) {
+        super(numberOfRows, numberOfColumns);
+
+		/* Initialize blocks cells. */
+        for (int row = 0; row < numberOfRows; row++)
+            for (int column = 0; column < numberOfColumns; column++)
+                setValueAt(new Integer(0), row, column);
+    }
+}
