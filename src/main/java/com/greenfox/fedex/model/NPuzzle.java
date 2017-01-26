cf4112c4f97ba921b30a561032b97131c1d6675f
viktornagy <vnagy2000@gmail.com>
2017-01-26 16:59:48 +0100

    plus Npuzzle

---
+++ src/main/java/com/greenfox/fedex/model/NPuzzle.java
@@ -0,0 +1,21 @@
+package com.greenfox.fedex.model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by Viktor on 2017.01.26..
+ */
+public class NPuzzle {
+
+    int numOfRows, numOfColumns;
+    int[][] picture;
+
+
+    public NPuzzle(int r, int c) {
+        numOfRows = r;
+        numOfColumns = c;
+
+    }
+}
